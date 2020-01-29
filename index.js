f3a81f47b303f422009edb18ddc62327ea5dccff
Tatiana <tatianaw83@gmail.com>
2020-01-29 13:48:34 -0800

    Updated

--- index.js
+++ index.js
@@ -51,6 +51,16 @@ app.post("/blogs", function(req,res){
         });
 }); 
 
+app.get("/blogs/:id", function(req,res){
+    Blog.findById(req.params.id, function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog:foundBlog})
+        }
+    })
+})
+
 app.listen(5500, process.env.IP, function(){
     console.log("Blog App Server")
 });
